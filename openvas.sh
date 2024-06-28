a405138a08e6bfd3bb02b6aad126f23fb891c7d8
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-06-28 14:23:57 +0530

    Update openvas.sh

--- openvas.sh
+++ openvas.sh
@@ -1,29 +1,29 @@
 # openvas
  sudo apt install docker.io
  sudo docker run -d -p 443:443 --name openvas mikesplain/openvas
- 
-# Function to display dragon ASCII art
+ # Function to display dragon ASCII art
 function draw_dragon {
     echo "                           / \\  //\\"
-    echo "               |\\___/|   /   \\//  \\\"
+    echo "               |\\___/|   /   \\//  \\\\"
     echo "               /0  0  \\__  /    //  | \\ \\"
     echo "              /     /  \\/_/    //   |  \\  \\"
-    echo "              \@_^_\@'/   \\/_   //    |   \\   \\"
+    echo "              \@_^_\\'/   \\/_   //    |   \\   \\"
     echo "              //_^_/     \\/_ //     |    \\    \\"
-    echo "           ( //) |        \\///      |     \\     \\"
+    echo "           ( //) |        \\\///      |     \\     \\"
     echo "         ( / /) _|_ /   )  //       |      \\     _\\"
     echo "       ( // /) '/,_ _ _/  ( ; -.    |    _ _\\.-~        .-~~~^-."
     echo "     (( / / )) ,-{        _      \`-.|.-~-.           .~         \`."
     echo "    (( // / ))  '/\\      /                 ~-. _ .-~      .-~^-.  \\"
-    echo "    (( /// ))      `.   {            }                   /      \\  \\"
+    echo "    (( /// ))      \`.   {            }                   /      \\  \\"
     echo "     (( / ))     .----~-.\\        \\-'                 .~         \\  \`."
-    echo "                ///.----..>        \\             _ -~            \`\\""\`."
+    echo "                ///.----..>        \\             _ -~            \`\\\"\`."
     echo "                  ///-._ _ _ _ _ _ _}^ - - - - ~                     \`"
 }
 
 # Main script execution
-echo "Here's a dragon for you:"
-draw_dragon 
+echo "openvas installation complete:"
+draw_dragon
+
  echo "installation complete"
  sleep 2
  sudo docker ps -a
