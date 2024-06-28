49f607f580dbfcc739c837db98fd77df1ac292df
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-06-28 14:19:01 +0530

    Update openvas.sh

--- openvas.sh
+++ openvas.sh
@@ -1,4 +1,31 @@
 # openvas
  sudo apt install docker.io
  sudo docker run -d -p 443:443 --name openvas mikesplain/openvas
+ 
+# Function to display dragon ASCII art
+function draw_dragon {
+    echo "                           / \\  //\\"
+    echo "               |\\___/|   /   \\//  \\\"
+    echo "               /0  0  \\__  /    //  | \\ \\"
+    echo "              /     /  \\/_/    //   |  \\  \\"
+    echo "              \@_^_\@'/   \\/_   //    |   \\   \\"
+    echo "              //_^_/     \\/_ //     |    \\    \\"
+    echo "           ( //) |        \\///      |     \\     \\"
+    echo "         ( / /) _|_ /   )  //       |      \\     _\\"
+    echo "       ( // /) '/,_ _ _/  ( ; -.    |    _ _\\.-~        .-~~~^-."
+    echo "     (( / / )) ,-{        _      \`-.|.-~-.           .~         \`."
+    echo "    (( // / ))  '/\\      /                 ~-. _ .-~      .-~^-.  \\"
+    echo "    (( /// ))      `.   {            }                   /      \\  \\"
+    echo "     (( / ))     .----~-.\\        \\-'                 .~         \\  \`."
+    echo "                ///.----..>        \\             _ -~            \`\\""\`."
+    echo "                  ///-._ _ _ _ _ _ _}^ - - - - ~                     \`"
+}
+
+# Main script execution
+echo "Here's a dragon for you:"
+draw_dragon 
+ echo "installation complete"
+ sleep 2
  sudo docker ps -a
+ echo "user-name: admin"
+ echo "password: admin" 
