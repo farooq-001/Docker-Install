5462bb744c86f0fe1132ceda5c63203880398db6
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-12-22 19:24:50 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -14,27 +14,6 @@ else
     exit 1
 fi
 
-# Function to install Docker
-install_docker() {
-    case $DISTRO in
-        ubuntu|debian)
-            sudo apt update
-            sudo apt install -y docker.io
-            sudo systemctl enable docker
-            sudo systemctl start docker
-            ;;
-        rocky|rhel|centos)
-            sudo yum install -y docker
-            sudo systemctl enable docker
-            sudo systemctl start docker
-            ;;
-        *)
-            echo "Unsupported distribution: $DISTRO"
-            exit 1
-            ;;
-    esac
-}
-
 # Function to install Docker Compose
 install_docker_compose() {
     DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
@@ -46,15 +25,7 @@ install_docker_compose() {
 LOG_FILE=~/docker-script-install.log
 
 # Main Installation Logic
-echo "Starting Docker and Docker Compose installation..." | tee -a $LOG_FILE
-
-# Install Docker
-if command_exists docker; then
-    echo "Docker is already installed." | tee -a $LOG_FILE
-else
-    echo "Installing Docker..." | tee -a $LOG_FILE
-    install_docker | tee -a $LOG_FILE
-fi
+echo "Starting Docker Compose installation..." | tee -a $LOG_FILE
 
 # Install Docker Compose
 if command_exists docker-compose; then
@@ -64,21 +35,10 @@ else
     install_docker_compose | tee -a $LOG_FILE
 fi
 
-# Check Docker and Docker Compose versions
-if command_exists docker; then
-    echo "Docker version:" | tee -a $LOG_FILE
-    docker --version | tee -a $LOG_FILE
-fi
-
+# Check Docker Compose version
 if command_exists docker-compose; then
     echo "Docker Compose version:" | tee -a $LOG_FILE
     docker-compose --version | tee -a $LOG_FILE
-fi
-
-# Check Docker service status
-if command_exists docker; then
-    echo "Checking Docker service status..." | tee -a $LOG_FILE
-    sudo systemctl status docker | tee -a $LOG_FILE
 fi
 
 # Create directories and setup Guacamole
